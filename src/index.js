bbb06faad0e4381157d9635f25ef426bbe798fe8
Mahmoud-sami-dev <mahmoudsaminassf@gmail.com>
2026-03-08 21:53:38 +0200

    new committ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { connectDB } from "./DB/connection.js";
 import { authRouter, userRouter } from "./modules/index.js";
-import { decryption, encryption } from "./common/utils/encryption.utils.js";
 const app = express();
 const port = 3000;
 
@@ -9,6 +8,7 @@ connectDB();
 
 //parsing data from req
 app.use(express.json());
+
 //routing
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
@@ -25,9 +25,6 @@ app.listen(port, () => {
   console.log("application is running on port", port);
 });
 
-// const encData = encryption("Mahmoud");
-// console.log({ encData });
-// const decData = decryption(
-//   "bed033eb1a31cb2608d0b6190df0000d:40c1bafe05a127097a5405056f7fc64d",
-// );
-// console.log(decData);
+
+
+//https://nassefm807-7801898.postman.co/workspace/Mahmoud-Sami-Nassef's-Workspace~2c8f71e3-56cd-43ad-874e-f48b10c4129c/collection/51125510-1793c4ac-77db-42ce-9e69-8b644beb144f?action=share&creator=51125510&active-environment=51125510-a6a01ba8-8aeb-4d6b-ac3f-555686cd98a7
